78a7e44e12e550902759011bd6d534a72689724c
Jeremy Brown <jeremyjjbrown@gmail.com>
2019-04-18 09:19:12 -0500

    mostly i3 updates

---
+++ 5510/pacman.sh
@@ -0,0 +1,88 @@
+   14  sudo pacman -S xfce4 network-manager-applet xorg-server xorg-xinit xorg-server-utils
+   15  sudo pacman -S xfce4 network-manager-applet xorg-server xorg-xinit 
+   23  sudo pacman -S numix-gtk-theme
+   26  sudo pacman -S nitrogen
+   29  pacman -S i3-gaps
+   30  sudo pacman -S i3-gaps
+   31  sudo pacman -S go python make 
+   32  sudo pacman -S git
+   41  sudo pacman -S openssh
+   48  sudo pacman -S firefox
+   75  sudo pacman -S ttf-font-awesome
+   80  sudo pacman -S i3_status
+   81  sudo pacman -S i3status
+   83  sudo pacman -S i3status
+   86  sudo pacman -S termite
+  125  sudo pacman -Syu
+  178  sudo pacman -S archey3
+  188  pacman -S scrot
+  195  sudo pacman -S scrot
+  199  pacman -S scrot
+  206  sudo pacman -S scrot
+  211  pacman -S openssl
+  212  sudo pacman -S openssl
+  213  sudo pacman -S openconnect
+  214  sudo pacman -S oauth-toolkit
+  215  sudo pacman -S oath-toolkit
+  217  sudo pacman -S tmux
+  235  sudo pacman -S dnsutils
+  256  sudo pacman -S networkmanager-openconnect
+  258  sudo pacman -S xclip
+  283  sudo pacman -S ttf-hack
+  297  sudo pacman -S networkmanager-openconnect
+  299  sudo pacman -S xclip
+  323  sudo pacman -S ttf-hack
+  333  sudo pacman -S gtk-theme-numix-solarized
+  360  sudo pacman -S nvim
+  361  sudo pacman -S neovim
+  372  sudo pacman -S python2.7
+  373  sudo pacman -S python2
+  394  sudo pacman -S rsync
+  397  sudo pacman -S python2.7
+  398  sudo pacman -S python2
+  415  sudo pacman -S rsync
+  461  sudo pacman -S xcape
+  479  sudo pacman -S tree
+  566  sudo pacman -S convert i3lock
+  567  sudo pacman -S  i3lock
+  568  sudo pacman -S imagemagick
+  609  sudo pacman -S libcanberra  libcanberra-pulse 
+  610  sudo pacman -S xfce4-pulseaudio-plugin
+  612  sudo pacman -S xfce4-mixer
+  615  sudo pacman -S libcanberra  libcanberra-pulse 
+  616  sudo pacman -S xfce4-pulseaudio-plugin
+  617  sudo pacman -S xfce4-mixer
+  621  sudo pacman -S pulse-audio
+  622  sudo pacman -S pulseaudio
+  623  sudo pacman -S alsamixer
+  624  sudo pacman -S pulseaudio-alsa
+  626  pacman -S alsa-utils
+  627  sudo pacman -S alsa-utils
+  630  sudo pacman -S asoundconf
+  642  sudo pacman -S polo
+  645  sudo pacman -S polo
+  677  sudo pacman -S dtrx
+  679  sudo pacman -S unzip
+  702  sudo pacman -S bzip2 wget
+  774  sudo pacman -S mesa bumblebee  xf86-video-intel 
+  786  pacman -S docker
+  787  sudo pacman -Syu
+  788  sudo pacman -S docker
+  798  sudo pacman -S nouveau
+  799  sudo pacman -S nvidea-settings
+  800  sudo pacman -S nvidia-settings
+  807  sudo pacman -S nvidia-dkms
+  817  sudo pacman -S nvidia-dkms
+  819  sudo pacman -S nvidia
+  856  sudo pacman -S nvidia-dkms
+  869  sudo pacman -S python2-pip
+  896  sudo pacman -S kubectl helm
+  959  sudo pacman -S nvidia-dkms
+  964  udo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
+  965  sudo pacman -S xf86-video-nouveau --noconfirm --needed
+  966  sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
+ 1036  sudo pacman -S nvidia-daocker
+ 1037  sudo pacman -S nvidia-docker
+ 1047  sudo pacman -S nvidia-daocker
+ 1048  sudo pacman -S nvidia-docker
+ 1118  history | grep "pacman -S" >> pacman.sh
